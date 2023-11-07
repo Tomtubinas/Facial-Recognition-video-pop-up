ebcfe844a234d346e9d380f48ec0fe216baea57e
Tomtubinas <143620957+Tomtubinas@users.noreply.github.com>
2023-11-07 15:21:06 -0500

    Update Camera.py
    full screens camera

--- Camera.py
+++ Camera.py
@@ -1,12 +1,22 @@
 import numpy as np
 import cv2
+import tkinter as tk
 # pylint: disable=E1101
 # runs on python 3.10.11
 
 cap = cv2.VideoCapture(0)
 
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.destroy()
+
+print(f"Monitor size: {screen_width}x{screen_height} pixels")
+
 while True:
     ret, frame = cap.read()
+
+    frame = cv2.resize(frame, (screen_width, screen_height))
 
     cv2.imshow('frame', frame)
 
