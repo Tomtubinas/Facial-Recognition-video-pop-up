4e2dd952859efd625cd62cb4816bb0bb65b25de7
Thomas Lomas <143426935+TLomasArcadia@users.noreply.github.com>
2023-11-27 22:43:22 -0500

    Messing around with detecting face and playing video
    So the video starts to play when the face is first not detected but it just stays there. The facial detection continues to run but the video just stays there.

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,26 +1,60 @@
 import cv2
-import numpy as np
-#ffpyplayer for playing audio
-from ffpyplayer.player import MediaPlayer
+import tkinter as tk
+import threading
 
-video_path = r'Path to File'
+scanFace = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
 
-def PlayVideo(video_path):
-    video=cv2.VideoCapture(video_path)
-    player = MediaPlayer(video_path)
+cap = cv2.VideoCapture(0)
+
+root = tk.Tk()
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+root.destroy()
+
+video_path = r'C:\Users\jaghe\Python\Final Project\Rick.mp4'
+
+def play_video():
+    video = cv2.VideoCapture(video_path)
     while True:
-        grabbed, frame=video.read()
-        audio_frame, val = player.get_frame()
-        if not grabbed:
+        ret, frame = video.read()
+        if not ret:
             print("End of video")
             break
+        cv2.imshow("Video", frame)
         if cv2.waitKey(28) & 0xFF == ord("q"):
             break
-        cv2.imshow("Video", frame)
-        if val != 'eof' and audio_frame is not None:
-            #audio
-            img, t = audio_frame
     video.release()
     cv2.destroyAllWindows()
-    
-PlayVideo(video_path)
+
+def detect_bounding_box(vid):
+    image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+    faces = scanFace.detectMultiScale(image, 1.1, 6, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (255, 0, 255), 4)
+    return faces
+
+video_playing = False
+
+while True:
+    ret, frame = cap.read()
+
+    frame = cv2.resize(frame, (screen_width, screen_height))
+    faces = detect_bounding_box(frame)
+
+    cv2.imshow('BEANS', frame)
+
+    if len(faces) == 0 and not video_playing:
+        video_playing = True
+        video_thread = threading.Thread(target=play_video)
+        video_thread.start()
+
+    elif len(faces) > 0:
+        video_playing = False
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
