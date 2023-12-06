6d775d174e955b584e3a451667a3638a9ed78857
Tomtubinas <143620957+Tomtubinas@users.noreply.github.com>
2023-12-06 10:20:05 -0500

    Update Camera.py
    Need to add delay

--- Camera.py
+++ Camera.py
@@ -1,8 +1,7 @@
-import numpy as np
 import cv2
+import time
 import tkinter as tk
-# pylint: disable=E1101
-# runs on python 3.10.11
+import threading
 
 scanFace = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
@@ -15,6 +14,21 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 root.destroy()
 
+video_path = r"Rick.mp4"
+
+def play_video():
+    video = cv2.VideoCapture(video_path)
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            print("End of video")
+            break
+        cv2.imshow("Video", frame)
+        if cv2.waitKey(28) & 0xFF == ord("q"):
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
 def detect_bounding_box(vid):
     image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
     faces = scanFace.detectMultiScale(image, 1.1, 6, minSize=(40, 40))
@@ -22,13 +36,27 @@ def detect_bounding_box(vid):
         cv2.rectangle(vid, (x, y), (x + w, y + h), (255, 0, 255), 4)
     return faces
 
+video_playing = False
+
 while True:
     ret, frame = cap.read()
+
+    startTime = 2
 
     frame = cv2.resize(frame, (screen_width, screen_height))
     faces = detect_bounding_box(frame)
 
     cv2.imshow('BEANS', frame)
+
+    if len(faces) == 0 and not video_playing:
+        video_playing = True
+        video_thread = threading.Thread(target=play_video)
+        video_thread.start()
+        cap.release()
+        cv2.destroyWindow('BEANS')
+
+    elif len(faces) > 0:
+        video_playing = False
 
     if cv2.waitKey(1) == ord('q'):
         break
